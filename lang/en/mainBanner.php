453ef5996a41eff22c2f40f214497668139f478d
Tr4shcoookieees <tr4shcoookieees@gmail.com>
2023-12-22 04:35:45 +0700

    chore(files): Удаление ненужных файлов

--- lang/en/mainBanner.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'first' => 'Buying art has become much easier',
-    'second' => ' is exactly the place you have been looking for!',
-    'last' => 'Free shipping, fair prices, over a million works from around the world for sale!',
-    'button' => 'Learn more'
-];
